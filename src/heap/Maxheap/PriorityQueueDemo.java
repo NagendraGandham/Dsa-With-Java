d451c30fe30c365147e0b37b1099f9a91780c472
Nagendra Gandham <nagendragandham770@gmail.com>
2026-06-22 19:32:52 +0530

    priority Queues

---
+++ src/heap/Maxheap/PriorityQueueDemo.java
@@ -0,0 +1,18 @@
+package heap.Maxheap;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Comparator.reverseOrder());
+        pq.add(1);
+        pq.offer(20);
+        pq.offer(10);
+        pq.offer(2);
+        System.out.println(pq);
+        pq.remove();
+        System.out.println(pq);
+
+    }
+}
