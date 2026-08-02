0df0f057b09dffb8efd34835f6813b2529a7e1f2
Nagendra Gandham <nagendragandham770@gmail.com>
2026-08-02 14:17:21 +0530

    Undirected weighted

--- src/Graphs/UndirectedAdjacencyList.java
+++ src/Graphs/UndirectedAdjacencyList.java
@@ -2,9 +2,23 @@ package Graphs;
 
 import java.util.ArrayList;
 
+class Pair{
+    int node;
+    int weight;
+    Pair(int node,int weight){
+        this.node=node;
+        this.weight=weight;
+    }
+
+    @Override
+    public String toString(){
+        return "("+node+","+weight+")";
+    }
+}
 
 public class UndirectedAdjacencyList {
-    private ArrayList<Integer>[] adjList;
+//    private ArrayList<Integer>[] adjList;
+    private ArrayList<Pair>[] adjList;
     private int vertices;
 
     UndirectedAdjacencyList(int vertices){
@@ -17,9 +31,9 @@ public class UndirectedAdjacencyList {
     }
 
 
-    public void addEdge(int u,int v){
-        adjList[u].add(v);
-        adjList[v].add(u);
+    public void addEdge(int u,int v,int weight){
+        adjList[u].add(new Pair(v,weight));
+        adjList[v].add(new Pair(u,weight));
     }
 
     public void printGraph(){
@@ -31,13 +45,13 @@ public class UndirectedAdjacencyList {
 
     public static void main(String[] args){
         UndirectedAdjacencyList graph=new UndirectedAdjacencyList(6);
-        graph.addEdge(0,1);
-        graph.addEdge(0,3);
-        graph.addEdge(1,2);
-        graph.addEdge(1,3);
-        graph.addEdge(2,3);
-        graph.addEdge(2,4);
-        graph.addEdge(3,4);
+        graph.addEdge(0,1,10);
+        graph.addEdge(0,3,3);
+        graph.addEdge(1,2,6);
+        graph.addEdge(1,3,78);
+        graph.addEdge(2,3,34);
+        graph.addEdge(2,4,35);
+        graph.addEdge(3,4,67);
         graph.printGraph();
     }
 
